676ad2725f92741725bff37ff0950586cdf8fc0e
s3877996 <69973205+s3877996@users.noreply.github.com>
2023-08-04 16:03:55 +0700

    Update DetailsView.swift

--- BehideTheArtistry/DetailsView.swift
+++ BehideTheArtistry/DetailsView.swift
@@ -5,6 +5,13 @@
 //  Created by Hien Nguyen Minh on 04/08/2023.
 //
 
+//
+//  DetailsView.swift
+//  BehideTheArtistry
+//
+//  Created by Hien Nguyen Minh on 04/08/2023.
+//
+
 import SwiftUI
 
 struct DetailsView: View {
@@ -12,14 +19,67 @@ struct DetailsView: View {
     
     var body: some View {
         
-        VStack{
-            Text(singer.stageName)
-            Text(singer.birthname)
-            Text(singer.birthplace)
-            Text(singer.genres)
-            Text(singer.fact)
+        VStack {
+            ZStack(alignment: .top){
+                Color.clear
+                
+                //var imageURL = singer.externalUrls.image
+                
+                // Display an image from a URL
+                if   let imageURL = URL(string: singer.externalUrls.image) {
+                    AsyncImage(url: imageURL) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                } else {
+                    Text("Image URL not found")
+                }
+                
+                
+            }
+            .frame(width: 340)
+            
+            VStack(alignment: .leading) {
+                Text("Information").font(.headline)
+                Spacer()
+                Text("Stage Name: " + singer.stageName)
+                    .font(
+                        Font.custom("Manrope", size: 16)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(Color(red: 0, green: 0.49, blue: 0.29))
+                Spacer()
+                Text("Birthplace name : " + singer.birthplace)
+                    .font(
+                        Font.custom("Manrope", size: 16)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(Color(red: 0, green: 0.49, blue: 0.29))
+                
+                Spacer()
+                Text("Genres: " + singer.genres)
+                    .font(
+                        Font.custom("Manrope", size: 16)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(Color(red: 0, green: 0.49, blue: 0.29))
+                Spacer()
+                Text("Fun facts: " + singer.fact + "\n").fixedSize(horizontal: false, vertical: false)
+                    .font(
+                        Font.custom("Manrope", size: 16)
+                            .weight(.bold)
+                    )
+                    .foregroundColor(Color(red: 0, green: 0.49, blue: 0.29))
+                Spacer()
+            }.frame(width: 340)
+            
             MapView(coordinate: singer.locationCoordinate2D)
-                .frame(height: 200)
+                .frame(width: 340,height: 250)
+            
+            
         }
         
     }
